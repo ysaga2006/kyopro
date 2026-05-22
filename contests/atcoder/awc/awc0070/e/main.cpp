04c33cfe16c8ad3ed07cb21ec75c66ebcabc32c8
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-05-22 09:12:34 +0900

    update: awc0070-e

--- contests/atcoder/awc/awc0070/e/main.cpp
+++ contests/atcoder/awc/awc0070/e/main.cpp
@@ -21,7 +21,8 @@ struct Dijkstra {
 
   void add_edge(int from, int to, Cost cost, bool undirected = false) {
     g[from].push_back({to, cost});
-    if (undirected) g[to].push_back({from, cost});
+    if (undirected)
+      g[to].push_back({from, cost});
   }
 
   vector<Cost> solve(int s, Cost inf = numeric_limits<Cost>::max() / 4) const {
@@ -32,7 +33,8 @@ struct Dijkstra {
     while (!pq.empty()) {
       auto [d, v] = pq.top();
       pq.pop();
-      if (d != dist[v]) continue;
+      if (d != dist[v])
+        continue;
       for (const auto& e : g[v]) {
         if (dist[e.to] > d + e.cost) {
           dist[e.to] = d + e.cost;
@@ -48,7 +50,25 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  ll n, m, k; cin >> n >> m >> k;
+  const ll INF = 1e18;
+
+  ll n, m, k;
+  cin >> n >> m >> k;
+
+  vector<ll> togo(k);
+  for (ll i = 0; i < k; ++i) {
+    cin >> togo[i]; togo[i]--;
+  }
+
+  Dijkstra<ll> di(n);
+  for (ll i = 0; i < n; ++i) {
+    ll a, b, c; cin >> a >> b >> c; a--; b--;
+    di.add_edge(a, b, c, true);
+  }
+
+  vector dist(n, vector<ll>(n, INF));
+
+  for (ll i = 0; i < k; ++i) di.solve(togo[i]);
 
 
   return 0;
