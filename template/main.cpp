3235fa83e69b196d684aeed9d9948025d4591d8f
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-05-24 13:15:27 +0900

    update: environment

--- template/main.cpp
+++ template/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
 
 int main() {
   ios::sync_with_stdio(false);
