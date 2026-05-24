32c19c937c07748c743b04b057cf23d735aa752f
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-05-24 11:39:10 +0900

    update: environment

--- template/local_compile.sh
+++ template/local_compile.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 src="${1:-template/main.cpp}"
 out="${2:-/tmp/a.out}"
 
-g++ -std=c++20 -O2 -Wall -Wextra -Wshadow -DLOCAL \
+g++ -std=c++23 -O2 -Wall -Wextra -Wshadow -DLOCAL \
     -I/Users/sagarayuto/kyopro \
     -I/Users/sagarayuto/kyopro/vendor/ac-library \
     "$src" -o "$out"
