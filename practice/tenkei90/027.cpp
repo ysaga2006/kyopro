05bfb93ae2327b2f0b2ee63814dbe0a7679a96fc
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-05-29 16:23:31 +0900

    ac: 027

---
+++ practice/tenkei90/027.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n; cin >> n;
+  set<string> st;
+
+  for (ll i = 0; i < n; ++i) {
+    string s; cin >> s;
+    if (!st.contains(s)) {
+      st.insert(s);
+      cout << i + 1 << '\n'; 
+    } 
+  }
+  return 0;
+}
