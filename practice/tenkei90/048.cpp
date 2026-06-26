c136af15f0079513fce4a4f361954779db844ab2
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-06-26 15:11:05 +0900

    add:048

---
+++ practice/tenkei90/048.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n, k;
+  cin >> n >> k;
+
+  
+  
+  return 0;
+}
