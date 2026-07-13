6fb82c852091491f987b98a219da91b0d964a16a
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-07-13 18:10:44 +0900

    ac:abc422-c

--- contests/atcoder/abc/abc4xx/abc422/c/main.cpp
+++ contests/atcoder/abc/abc4xx/abc422/c/main.cpp
@@ -1,19 +1,34 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+const ll INF = 10000000000;
 
 int main() {
-    int t;
-    cin >> t;
-    rep(i, t) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        if (a > c) {
-            cout << min(b + (a - c), c) << endl;
-        }
-        else {
-            cout << min(b + (c - a), a) << endl;
-        }
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll t; cin >> t;
+  rep(ti, t) {
+    ll a, b, c; cin >> a >> b >> c;
+    auto f = [&](ll x) -> bool {
+      if (a < x || c < x) return false;
+      else if ((a - x) + (c - x) + b < x) return false;
+      return true;
+    };
+
+    ll ok = 0, ng = a + b + c;
+    while (ng - ok > 1) {
+      ll mid = (ok + ng) / 2;
+      if (f(mid) == true) {
+        ok = mid;
+      } else {
+        ng = mid;
+      }
     }
-    return 0;
+
+    cout << ok << '\n';
+  }
+  return 0;
 }
