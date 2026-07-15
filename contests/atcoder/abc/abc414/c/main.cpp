5acf2330c678305e6c5084127cd72b2a0ec40b8a
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-07-15 11:56:44 +0900

    update: abc414-c

--- contests/atcoder/abc/abc414/c/main.cpp
+++ contests/atcoder/abc/abc414/c/main.cpp
@@ -7,5 +7,53 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
+  ll ans = 0;
+
+  ll a, n;
+  cin >> a >> n;
+
+  for (ll i = 1; i < 1000000; i++) {
+    auto f = [&](ll x) -> string {
+      string str = "";
+      while (x) {
+        str.push_back('0' + x % a);
+        x /= a;
+      }
+      reverse(str.begin(), str.end());
+      return str;
+    };
+
+    string s = to_string(i);
+    string res = s;
+    reverse(res.begin(), res.end());
+
+    ll even = stoll(s + res);
+    if (even > n) continue;
+
+    bool ispali = true;
+    string val = f(even);
+
+    rep(k, (val.size() / 2)) {
+      if (val[k] != val[val.size() - k - 1]) ispali = false;
+    }
+
+    if (ispali == true) ans += even, cout << even << " " << val << '\n';
+
+    for (ll j = 0; j <= 9; j++) {
+      ispali = true;
+      ll odd = stoll(s + to_string(j) + res);
+      if (odd > n) continue;
+
+      val = f(odd);
+      rep(k, (val.size() - 1) / 2) {
+        if (val[k] != val[val.size() - k - 1]) ispali = false;
+      }
+
+      if (ispali == true) ans += odd, cout << odd << " " << val << '\n';
+    }
+  }
+
+  cout << ans << '\n';
+
   return 0;
 }
