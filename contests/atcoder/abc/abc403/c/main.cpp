7c622cff71f6bca8d43385486841b07494a2b4ad
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-07-15 12:08:37 +0900

    ac: abc403-c

--- contests/atcoder/abc/abc403/c/main.cpp
+++ contests/atcoder/abc/abc403/c/main.cpp
@@ -7,5 +7,31 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
+  ll n, m, q;
+  cin >> n >> m >> q;
+  map<ll, set<ll>> mp;  // mp[i] = ページiが見れる人
+  set<ll> st;           // 全部行ける人
+
+  rep(qi, q) {
+    ll op;
+    cin >> op;
+    if (op == 1) {
+      ll x, y;
+      cin >> x >> y;
+      mp[y].insert(x);
+    }
+    if (op == 2) {
+      ll x;
+      cin >> x;
+      st.insert(x);
+    }
+    if (op == 3) {
+      ll x, y;
+      cin >> x >> y;
+      if (st.contains(x)) cout << "Yes" << '\n';
+      else if (mp[y].contains(x)) cout << "Yes" << '\n';
+      else cout << "No" << '\n';
+    }
+  }
   return 0;
 }
