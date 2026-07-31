18b472c0f69b98179bd1bc08769f4e21ce514f93
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-07-31 14:56:53 +0900

    ac: 002

--- practice/tenkei90/002.cpp
+++ practice/tenkei90/002.cpp
@@ -1,46 +1,49 @@
 #include <bits/stdc++.h>
-using ll = long long;
 using namespace std;
+using ll = long long;
+constexpr ll INF = (1LL << 60);
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  int n;
+
+  ll n;
   cin >> n;
-  set<string> st;
-  st.insert("()");
+
   set<string> ans;
-  if (n == 2) {
-    ans.insert("()");
-  } else if (n % 2 != 0) {
-    return 0;
-  } else {
-    int cnt = 0;
-    while (cnt < (1 << (n - 3)) + 1) {
-      for (auto ele : st) {
-        string can1 = '(' + ele + ')';
-        if (int(can1.size()) == n) {
-          ans.insert(can1);
-          cnt++;
-        } else if (int(can1.size()) < n) {
-          st.insert(can1);
-          cnt++;
-        }
-        for (auto elee : st) {
-          string can2 = ele + elee;
-          if (int(can2.size()) == n) {
-            ans.insert(can2);
-            cnt++;
-          } else if (int(can2.size()) < n) {
-            st.insert(can2);
-            cnt++;
-          }
-        }
+
+  auto good = [&](string x) -> bool {
+    stack<char> st;
+    for (char& c : x) {
+      if (!st.empty()) {
+        char tp = st.top();
+        if (tp == '(' && c == ')') st.pop();
+        else st.push(c);
+      } else {
+        st.push(c);
       }
     }
+
+    if (st.empty()) return true;
+    else return false;
+  };
+
+  for (ll bit = 0; bit < (1 << n); bit++) {
+    string s = "";
+    for (ll i = 0; i < n; i++) {
+      if (bit & (1 << i)) {
+        s.push_back('(');
+      } else {
+        s.push_back(')');
+      }
+    }
+
+    if (good(s) == true) ans.insert(s);
   }
-  for (auto s : ans) {
-    cout << s << endl;
+
+  for (string x : ans) {
+    cout << x << '\n';
   }
   return 0;
 }
