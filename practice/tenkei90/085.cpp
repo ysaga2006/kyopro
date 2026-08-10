3aa8579fd68edbad5b56d0046ba5f12f9f17d13b
ysaga2006 <sagara.y.4991@m.isct.ac.jp>
2026-08-10 15:10:09 +0900

    ac: 085

--- practice/tenkei90/085.cpp
+++ practice/tenkei90/085.cpp
@@ -7,7 +7,19 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  ll k; cin >> k;
-  
+  ll k;
+  cin >> k;
+
+  ll ans = 0;
+  for (ll i = 1; i * i * i <= k; i++) {
+    if (k % i != 0) continue;
+    for (ll j = i; j * j <= (k / i) + 5; j++) {
+      if ((k / i) % j != 0) continue;
+      if ((k / i / j) < j) continue;
+      ans++;
+    }
+  }
+
+  cout << ans << '\n';
   return 0;
 }
